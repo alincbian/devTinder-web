377e65f56868eab55bddb41b89472147739a2921
Ali Raza <alincbian@gmail.com>
2025-07-23 01:53:49 +0500

    feat: sign up and fix bugs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,8 +6,11 @@ import { BASE_URL } from "../utils/contstants";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("aliraza@gmail.com");
-  const [password, setPassword] = useState("Ali@3221");
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isLoginForm, setIsLoginForm] = useState(true);
   const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -29,11 +32,58 @@ const Login = () => {
     }
   };
 
+  const handleSignUp = async () => {
+    try {
+      const res = await axios.post(
+        `${BASE_URL}signup`,
+        {
+          firstName,
+          lastName,
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res?.data?.user));
+      navigate("/profile");
+    } catch (err) {
+      setError(err?.response?.data?.message || "Something went wrong!");
+    }
+  };
+
   return (
     <div className="flex justify-center items-center my-10">
       <div className="card bg-base-300 w-96 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title justify-center">Login</h2>
+          <h2 className="card-title justify-center">
+            {isLoginForm ? "Login" : "Sign Up"}
+          </h2>
+          {!isLoginForm && (
+            <>
+              <label className="form-control w-full max-w-xs my-2">
+                <div className="label">
+                  <span className="label-text">First Name</span>
+                </div>
+                <input
+                  type="text"
+                  value={firstName}
+                  className="input input-bordered w-full max-w-xs"
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </label>
+              <label className="form-control w-full max-w-xs my-2">
+                <div className="label">
+                  <span className="label-text">Last Name</span>
+                </div>
+                <input
+                  type="text"
+                  value={lastName}
+                  className="input input-bordered w-full max-w-xs"
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </label>
+            </>
+          )}
           <label className="form-control w-full max-w-xs my-2">
             <div className="label">
               <span className="label-text">Email Id</span>
@@ -58,10 +108,21 @@ const Login = () => {
           </label>
           <p className="text-red-500">{error}</p>
           <div className="card-actions justify-center m-2">
-            <button className="btn btn-primary" onClick={handleLogin}>
-              Login
+            <button
+              className="btn btn-primary"
+              onClick={isLoginForm ? handleLogin : handleSignUp}
+            >
+              {isLoginForm ? "Login" : "Sign Up"}
             </button>
           </div>
+          <p
+            className="m-auto cursor-pointer py-2"
+            onClick={() => setIsLoginForm((value) => !value)}
+          >
+            {isLoginForm
+              ? "New User? Signup Here"
+              : "Existing User? Login Here"}
+          </p>
         </div>
       </div>
     </div>
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -3,9 +3,11 @@ import { removeFeed } from "../utils/feedSlice";
 import { BASE_URL } from "../utils/contstants";
 import axios from "axios";
 import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
 
 const UserCard = ({ user }) => {
   const dispatch = useDispatch();
+  const location = useLocation();
 
   const { id, firstName, lastName, photoUrl, about, age, gender } = user;
 
@@ -29,20 +31,22 @@ const UserCard = ({ user }) => {
         <h2 className="card-title">{firstName + " " + lastName}</h2>
         {age && gender && <p>{age + ", " + gender}</p>}
         <p>{about}</p>
-        <div className="card-actions justify-center my-4">
-          <button
-            className="btn btn-primary"
-            onClick={() => handleSendRequest("ignored", id)}
-          >
-            Ignore
-          </button>
-          <button
-            className="btn btn-secondary"
-            onClick={() => handleSendRequest("interested", id)}
-          >
-            Interested
-          </button>
-        </div>
+        {!location?.pathname?.includes("profile") && (
+          <div className="card-actions justify-center my-4">
+            <button
+              className="btn btn-primary"
+              onClick={() => handleSendRequest("ignored", id)}
+            >
+              Ignore
+            </button>
+            <button
+              className="btn btn-secondary"
+              onClick={() => handleSendRequest("interested", id)}
+            >
+              Interested
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
